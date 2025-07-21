49ab2c4adab75a2a157cc1199ef03f53e1a78704
kanhamao <maokanha8888@gmail.com>
2025-07-21 13:00:01 +0700

    feat/(Navbar): navbar Logo Lotus Outreach[LOC-55]

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,14 @@
 <template>
   <div >
-    <Navbar />
+    <RouterView />
+    
+    
 
-    
-    <Footer/>
-    
-   
   </div> 
 
 </template>
 
 <script setup>
-import Navbar from './components/Navbar.vue';
-import Footer from './components/Footer.vue';
 
 
 import { RouterView } from 'vue-router';
