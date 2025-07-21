6627133cdbe2335c89c4461608701b7ce62da42e
kanhamao <maokanha8888@gmail.com>
2025-07-21 09:23:39 +0700

    navbar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <template>
-  <router-view />
-</template>
+  <div >
+    <Navbar />
+    
+   
+  </div> 
 
+</template>
 <script setup>
+import Navbar from './components/Navbar.vue';
+
+
+import { RouterView } from 'vue-router';
 </script>
