3dac1988d9aff912ac877e3a84136fa676078260
kanhamao <maokanha8888@gmail.com>
2025-07-22 15:50:06 +0700

    feat/(Footer): footer program update[LOC-58]

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -84,84 +84,86 @@
 
 
         <!-- Programs -->
-        <div>
-          <h4 class="text-lg font-semibold mb-3 text-blue-600">Our Programs</h4>
-          <ul class="space-y-3">
-            <li>
-              <a
-                href="#"
-                class="text-gray-600 hover:text-pink-600 transition-colors duration-300 flex items-center group"
-              >
-                <svg
-                  class="w-4 h-4 mr-2 text-pink-500"
-                  fill="currentColor"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    d="M12 3L1 9l4 2.18v6L12 21l7-3.82v-6l2-1.09V17h2V9L12 3zm6.82 6L12 12.72 5.18 9 12 5.28 18.82 9zM17 15.99l-5 2.73-5-2.73v-3.72L12 15l5-2.73v3.72z"
-                  />
-                </svg>
-                Education
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                class="text-gray-600 hover:text-pink-600 transition-colors duration-300 flex items-center group"
-              >
-                <svg
-                  class="w-4 h-4 mr-2 text-pink-500"
-                  fill="currentColor"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    d="M19 8l-4 4h3c0 3.31-2.69 6-6 6-1.01 0-1.97-.25-2.8-.7l-1.46 1.46C8.97 19.54 10.43 20 12 20c4.42 0 8-3.58 8-8h3l-4-4zM6 12c0-3.31 2.69-6 6-6 1.01 0 1.97.25 2.8.7l1.46-1.46C15.03 4.46 13.57 4 12 4c-4.42 0-8 3.58-8 8H1l4 4 4-4H6z"
-                  />
-                </svg>
-                Care
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                class="text-gray-600 hover:text-pink-600 transition-colors duration-300 flex items-center group"
-              >
-                <svg
-                  class="w-4 h-4 mr-2 text-pink-500"
-                  fill="currentColor"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"
-                  />
-                </svg>
-                Training
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                class="text-gray-600 hover:text-pink-600 transition-colors duration-300 flex items-center group"
-              >
-                <svg
-                  class="w-4 h-4 mr-2 text-pink-500"
-                  fill="currentColor"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 
-                          2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 
-                          4.5 2.09C13.09 3.81 14.76 3 
-                          16.5 3 19.58 3 22 5.42 22 
-                          8.5c0 3.78-3.4 6.86-8.55 
-                          11.54L12 21.35z"
-                  />
-                </svg>
-                Giving Back
-              </a>
-            </li>
-          </ul>
-        </div>
+       
+  <div>
+    <h4 class="text-lg font-semibold mb-3 text-blue-600">Our Programs</h4>
+    <ul class="space-y-3">
+      <li>
+        <router-link
+          to="/program"
+          class="text-gray-600 hover:text-pink-600 transition-colors duration-300 flex items-center group"
+        >
+          <svg
+            class="w-4 h-4 mr-2 text-pink-500"
+            fill="currentColor"
+            viewBox="0 0 24 24"
+          >
+            <path
+              d="M12 3L1 9l4 2.18v6L12 21l7-3.82v-6l2-1.09V17h2V9L12 3zm6.82 6L12 12.72 5.18 9 12 5.28 18.82 9zM17 15.99l-5 2.73-5-2.73v-3.72L12 15l5-2.73v3.72z"
+            />
+          </svg>
+          Education
+        </router-link>
+      </li>
+      <li>
+        <router-link
+          to="/program"
+          class="text-gray-600 hover:text-pink-600 transition-colors duration-300 flex items-center group"
+        >
+          <svg
+            class="w-4 h-4 mr-2 text-pink-500"
+            fill="currentColor"
+            viewBox="0 0 24 24"
+          >
+            <path
+              d="M19 8l-4 4h3c0 3.31-2.69 6-6 6-1.01 0-1.97-.25-2.8-.7l-1.46 1.46C8.97 19.54 10.43 20 12 20c4.42 0 8-3.58 8-8h3l-4-4zM6 12c0-3.31 2.69-6 6-6 1.01 0 1.97.25 2.8.7l1.46-1.46C15.03 4.46 13.57 4 12 4c-4.42 0-8 3.58-8 8H1l4 4 4-4H6z"
+            />
+          </svg>
+          Care
+        </router-link>
+      </li>
+      <li>
+        <router-link
+          to="/program"
+          class="text-gray-600 hover:text-pink-600 transition-colors duration-300 flex items-center group"
+        >
+          <svg
+            class="w-4 h-4 mr-2 text-pink-500"
+            fill="currentColor"
+            viewBox="0 0 24 24"
+          >
+            <path
+              d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"
+            />
+          </svg>
+          Training
+        </router-link>
+      </li>
+      <li>
+        <router-link
+          to="/donate"
+          class="text-gray-600 hover:text-pink-600 transition-colors duration-300 flex items-center group"
+        >
+          <svg
+            class="w-4 h-4 mr-2 text-pink-500"
+            fill="currentColor"
+            viewBox="0 0 24 24"
+          >
+            <path
+              d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 
+                      2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 
+                      4.5 2.09C13.09 3.81 14.76 3 
+                      16.5 3 19.58 3 22 5.42 22 
+                      8.5c0 3.78-3.4 6.86-8.55 
+                      11.54L12 21.35z"
+            />
+          </svg>
+          Giving Back
+        </router-link>
+      </li>
+    </ul>
+  </div>
+
 
         <!-- Connect With Us -->
         <div>
