cdafaa645de6f16c14a5bf386460c39cd915410b
MAO.KANHA <MAO.KANHA@PNC.LOCAL>
2025-08-06 09:07:51 +0700

    feat/(Responsive): Responsive Header Contact Page[LOC-123]

--- src/components/contact/Header.vue
+++ src/components/contact/Header.vue
@@ -4,7 +4,7 @@
     <img
       src="/image/contact/header.jpg"
       alt="Contact Us - Cambodia"
-      class="w-full h-[28rem] md:h-[36rem] lg:h-[40rem] 2xl:h-[44rem] object-cover object-center"
+      class="w-full h-[25rem] md:h-[36rem] lg:h-[40rem] 2xl:h-[44rem] object-cover object-center"
       loading="eager"
     />
 
@@ -12,27 +12,26 @@
     <div class="absolute inset-0 bg-black/30 backdrop-brightness-75 flex items-center justify-center"></div>
 
     <!-- Centered Content -->
-    <div class="absolute inset-0 z-10 flex flex-col items-center justify-center text-center px-6">
-      <!-- Heading -->
-      <h1 class="text-4xl sm:text-4xl lg:text-6xl font-bold text-white mb-1 sm:mb-6 leading-snug sm:leading-tight">
-     Contact With Us
-        
-    </h1>
+<div class="absolute inset-0 z-10 flex flex-col items-center justify-center text-center px-6">
+  <!-- Heading -->
+  <h1 class="text-2xl sm:text-4xl lg:text-6xl font-bold text-white mb-1 sm:mb-6 leading-snug sm:leading-tight">
+    Contact With Us
+  </h1>
+
+  <!-- Subheading -->
+<p class="text-sm sm:text-lg md:text-xl lg:text-2xl max-w-3xl text-gray-200 font-medium drop-shadow-md mb-4">
+  Reach out and join our mission to empower Cambodian girls and uplift rural communities through education and opportunity.
+</p>
 
 
-      <!-- Subheading -->
-      <p class="text-lg sm:text-xl md:text-2xl max-w-3xl text-gray-200 font-medium drop-shadow-md mb-8">
-        Reach out and join our mission to empower Cambodian girls and uplift rural communities through education and opportunity.
-      </p>
+  <!-- Donate Button -->
+  <router-link
+    to="/donate"
+    class="inline-flex items-center gap-2 bg-pink-600 hover:bg-pink-700 text-white px-4 sm:px-6 py-2 sm:py-3 rounded-full text-sm sm:text-lg font-semibold shadow-xl transition duration-300 ease-in-out transform hover:scale-105"
+  >
+    Donate Now
+  </router-link>
+</div>
 
-      <!-- Donate Button -->
-      <router-link
-        to="/donate"
-        class="inline-flex items-center gap-2 bg-pink-600 hover:bg-pink-700 text-white px-6 py-3 rounded-full text-lg font-semibold shadow-xl transition duration-300 ease-in-out transform hover:scale-105"
-      >
-      
-        Donate Now
-      </router-link>
-    </div>
   </div>
 </template>
